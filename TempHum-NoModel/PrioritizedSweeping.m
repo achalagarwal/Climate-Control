1a4d796594c15edc67882d76a5702400e1c52d0b
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-03-25 17:19:53 +0530

    Prioritized Sweeping implemented

---
+++ TempHum-NoModel/PrioritizedSweeping.m
@@ -0,0 +1,45 @@
+function Q  = PrioritizedSweeping(Q, Model, s, a, r, sp, alpha, gamma, theta, pqueue)
+% UpdateQ: update the Qtable and return it using a Prioritized Sweep of the
+% state-action space
+% s: previous state before taking action (a)
+% sp: current state after action (a)
+% r: reward received from the environment after taking action (a) in state
+%   s and reaching the state sp
+% a:  the last executed action
+% alpha: learning rate
+% gamma: discount factor
+% theta: the threshold for change in update value p, above which a
+%   state-action pair will be put in the queue of updating the QTable
+% Q: the resulting Qtable
+% Current estimated model of the environment
+% p: The update value towards which a backup is shifted
+
+% if (r ~= -1)
+%     theta = theta + 0.8*abs(r);
+% end
+
+p = abs((r + gamma*max(Q(sp,:))) - Q(s,a));
+    if (p > theta)
+        pqueue.push(1/p,[s a]);
+    end
+
+max_updates = 100;
+while(pqueue.size ~= 0 && max_updates)
+    sapair = pqueue.pop();
+    s = sapair(1);
+    a = sapair(2);
+    
+    sp = Model(s,a,1);
+    r = Model(s,a,2);
+    Q = UpdateQLearning( s, a, r, sp, Q , alpha, gamma);
+    
+    [sbar, abar] = find(Model(:,:,1) == s);
+    for i = 1:length(sbar)
+        rbar = Model(sbar(i),abar(i),2);
+        p = abs(rbar + gamma*max(Q(s,:)) - Q(sbar(i),abar(i)));
+        if (p > theta)
+            pqueue.push(1./p,[sbar(i) abar(i)]);
+        end
+    end
+    max_updates = max_updates - 1;    
+end
