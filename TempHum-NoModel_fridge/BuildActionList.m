fcbe4a5f27f9ced54f1e9742c635014e4ceef9a6
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-05-17 06:21:31 +0530

    Action List for fridge with bulbs

---
+++ TempHum-NoModel_fridge/BuildActionList.m
@@ -0,0 +1,11 @@
+function [ actions ] = BuildActionList
+%BuildActionList
+% 1 = bulb1_on, 
+% 2 = bulb1_off,
+% 3 = bulb2_on,
+% 4 = bulb2_off,
+% 5 = do nothing
+
+
+%action numbers
+actions = [1 2 3 4 5];
